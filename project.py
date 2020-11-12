908459595c37b1980cc4f31b460a0501907d7e11
yaofenglu <43899545+yaofenglu@users.noreply.github.com>
2020-11-11 23:55:57 -0500

    Update project.py

--- project.py
+++ project.py
@@ -41,10 +41,29 @@ def record_audio(ask=False):
 
 #get string and make a audio file to be played
 def speak(audio_string):
-    engine = pyttsx3.init()
-    engine.say(audio_string)
-    print(f"Rocket: {audio_string}")
-    engine.runAndWait()
+    speech_key, service_region = "61b8a438ab6e4afa8d7496ab6982d4e3", "eastus"
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+    # Creates a speech synthesizer using the default speaker as audio output.
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+    # Receives a text.
+    text = audio_string
+
+    # Synthesizes the received text to speech.
+    # The synthesized speech is expected to be heard on the speaker with this line executed.
+    result = speech_synthesizer.speak_text_async(text).get()
+
+    # Checks result.
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print("Speech synthesized to speaker for text [{}]".format(text))
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            if cancellation_details.error_details:
+                print("Error details: {}".format(cancellation_details.error_details))
+        print("Did you update the subscription info?")
 
 #LUIS
 def ai(voice_data):
