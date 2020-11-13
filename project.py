f3b4adb9a764cf282e229490f2d3bcf237d300b3
alexander-schifko15 <60859717+alexander-schifko15@users.noreply.github.com>
2020-11-13 00:31:45 -0500

    Update project.py

--- project.py
+++ project.py
@@ -12,6 +12,10 @@ import webbrowser
 import azure.cognitiveservices.speech as speechsdk
 import mysql.connector
 from mysql.connector import errorcode
+import sounddevice as sd
+import soundfile as sf
+from_language, to_languages = 'en-US', [ 'de', 'en', 'it', 'pt', 'zh-Hans' ]
+speech_key, service_region = "61b8a438ab6e4afa8d7496ab6982d4e3", "eastus"
 
 #listen for auio and convert it to text:
 def record_audio(ask=False):
@@ -274,7 +278,51 @@ def weather_API (city_name):
     p = "Right now in " + city_name + " the temperature is " + str(current_temperature) + " fahrenheit with " + weather_description + " higher " + str(max_temperature) + " degree and lower " + str(min_temperature) +" degree"
     return p
 
+def translate_speech_to_text():
+    translation_config = speechsdk.translation.SpeechTranslationConfig(
+            subscription=speech_key, region=service_region)
+
+    translation_config.speech_recognition_language = from_language
+    for lang in to_languages:
+        translation_config.add_target_language(lang)
+
+    recognizer = speechsdk.translation.TranslationRecognizer(
+            translation_config=translation_config)
     
+    print('Say something...')
+    result = recognizer.recognize_once()
+    synthesize_translations(result=result) 
+    play_translation(result=result)
+
+
+def synthesize_translations(result):
+    language_to_voice_map = {
+        "de": "de-DE-KatjaNeural",
+        "en": "en-US-AriaNeural",
+        "it": "it-IT-ElsaNeural",
+        "pt": "pt-BR-FranciscaNeural",
+        "zh-Hans": "zh-CN-XiaoxiaoNeural"
+    }
+    print(f'Recognized: "{result.text}"')
+
+    for language in result.translations:
+        translation = result.translations[language]
+        print(f'Translated into "{language}": {translation}')
+
+        speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+        speech_config.speech_synthesis_voice_name = language_to_voice_map.get(language)
+        
+        audio_config = speechsdk.audio.AudioOutputConfig(filename=f'{language}-translation.wav')
+        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+        speech_synthesizer.speak_text_async(translation).get()
+       
+
+def play_translation(result):
+        filename = "zh-Hans-translation.wav"
+        data, fs = sf.read(filename, dtype='float32')  
+        sd.play(data, fs)
+        status = sd.wait() 
+
 
 def Skills(response):
     text = response["query"]
@@ -324,6 +372,10 @@ def Skills(response):
     elif (intent == "get_time"):
         strTime = datetime.datetime.now().strftime("%H:%M:%S")
         speak(strTime)
+    
+    #translate
+    elif (intent == "get_translation"):
+        translate_speech_to_text()
         
 
     #
