8a03d234ff0b12d4d5d8c2700e438bf7c55f08ec
alexander-schifko15 <60859717+alexander-schifko15@users.noreply.github.com>
2020-11-12 22:19:33 -0500

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,59 @@
+
+import azure.cognitiveservices.speech as speechsdk
+import sounddevice as sd
+import soundfile as sf
+
+
+speech_key, service_region = "61b8a438ab6e4afa8d7496ab6982d4e3", "eastus"
+from_language, to_languages = 'zh-Hans', [ 'de', 'en', 'it', 'pt', 'zh-Hans' ]
+
+def translate_speech_to_text():
+    translation_config = speechsdk.translation.SpeechTranslationConfig(
+            subscription=speech_key, region=service_region)
+
+    translation_config.speech_recognition_language = from_language
+    for lang in to_languages:
+        translation_config.add_target_language(lang)
+
+    recognizer = speechsdk.translation.TranslationRecognizer(
+            translation_config=translation_config)
+    
+    print('Say something...')
+    result = recognizer.recognize_once()
+    synthesize_translations(result=result) 
+    play_translation(result=result)
+
+
+def synthesize_translations(result):
+    language_to_voice_map = {
+        "de": "de-DE-KatjaNeural",
+        "en": "en-US-AriaNeural",
+        "it": "it-IT-ElsaNeural",
+        "pt": "pt-BR-FranciscaNeural",
+        "zh-Hans": "zh-CN-XiaoxiaoNeural"
+    }
+    print(f'Recognized: "{result.text}"')
+
+    for language in result.translations:
+        translation = result.translations[language]
+        print(f'Translated into "{language}": {translation}')
+
+        speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+        speech_config.speech_synthesis_voice_name = language_to_voice_map.get(language)
+        
+        audio_config = speechsdk.audio.AudioOutputConfig(filename=f'{language}-translation.wav')
+        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+        speech_synthesizer.speak_text_async(translation).get()
+       
+
+def play_translation(result):
+        filename = "zh-Hans-translation.wav"
+        data, fs = sf.read(filename, dtype='float32')  
+        sd.play(data, fs)
+        status = sd.wait() 
+
+ 
+
+
+
+translate_speech_to_text()
